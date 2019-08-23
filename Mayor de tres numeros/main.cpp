cf7d7817a10dfc32838cf226d3156a31c797d174
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2019-08-23 09:27:22 -0500

    implementacion m3n

--- Mayor de tres numeros/main.cpp
+++ Mayor de tres numeros/main.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a;
+    int b;
+    int c;
+    cout << "numero a: " << endl;
+    cin >> a;
+    cout << "numero b: " << endl;
+    cin >> b;
+    cout << "numero c: " << endl;
+    cin >> c;
+    if (a >= b and a >= c)
+        cout << "el mayor es " << a << endl;
+
+    if (b >= a and b >= c)
+        cout << "el mayor es " << b << endl;
+
+    if (c >= a and c >=b)
+        cout << "el mayor es " << a << endl;
+
     return 0;
 }
