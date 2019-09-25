f7b3f99186c280893d7eda25c8e6bca570534b82
renatolazo <54364273+renatolazo@users.noreply.github.com>
2019-09-25 10:02:55 -0500

    https://github.com/renatolazo/Trabajo1.git

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+/*void imprimir(int arr[], int tam)
+{
+    for(int i = 0; i < tam; i++)
+        cout << arr[i] << endl;
+}
+
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    imprimir(arr, 5);
+}*/
+
+/*int sumar(int arr[], int tam)
+{
+    int suma = 0;
+    for(int i = 0; i < tam; i++)
+        suma += arr[i];
+    return suma;
+}
+
+int main()
+{
+    int arr[4] = {6, 14, 8, 18};
+    cout << sumar(arr, 4);
+}*/
+
+/*int sumar2(int arr[], int tam)
+{
+    int suma = 0;
+    int i = 0;
+    while(i < 5)
+        suma += arr[i++];
+    cout << suma << endl;
+}
+
+int main()
+{
+    int arr[5] = {2, 4, 18, 7, 13};
+    sumar2(arr, 5);
+}*/
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    swap(a, b);
+    cout << a << endl;
+    cout << b << endl;
+    return 0;
+}
